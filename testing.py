78ad55503e2ea3a750ab520a1cca79fd58460504
Jonathan <jonsecu0001@hotmail.com>
2017-11-10 13:34:47 -0600

    Probando codigo para graficar porcentaje de veces que se queda sin presupuesto.

--- testing.py
+++ testing.py
@@ -4,9 +4,9 @@ import numpy as np
 import sys
 import pandas as pd
 
-num_episodes = 20
-budgets = [10]
-reps = 2
+num_episodes = 100
+budgets = [100, 60, 20]
+reps = 5
 
 results = []
 
@@ -59,11 +59,11 @@ def load_stats(file_name='test.npy'):
     return stats_array
 
 
-# for b in budgets:
-#     run_episodes({'budget': b, 'a': 1})
+for b in budgets:
+    run_episodes({'budget': b, 'a': 1})
 
-filename = 'a-1_budget-080'
-
-stats_array_loaded = load_stats(filename)
-
-plotting.plot_episode_stats(stats_array_loaded, label=filename, smoothing_window=10)
+# filename = 'a-1_budget-080'
+#
+# stats_array_loaded = load_stats(filename)
+#
+# plotting.plot_episode_stats(stats_array_loaded, label=filename, smoothing_window=10)
