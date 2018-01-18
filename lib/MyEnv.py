a67df0671b0613b14dfeccd15fd0632dfbf0da27
Jonathan <jonsecu0001@hotmail.com>
2018-01-18 17:27:53 -0600

    Exp4.

--- lib/MyEnv.py
+++ lib/MyEnv.py
@@ -64,17 +64,17 @@ class MyEnv:
 
         # SALIDA ANTICIPADA ###################################################
         # permite terminar antes si se cumplen condiciones de posicion y accion
-        # if +0.60 <= action[0] < +0.90:
-        #     position = next_state[0]
-        #     if -0.10 <= position < 0.25:
-        #         reward = 40 - math.pow(action[0], 2) * 0.1
-        #         # terminacion por salida anticipada
-        #         print reward
-        #         self.set_done_reason(2)
-        #     else:
-        #         pass
-        # else:
-        #     pass
+        if +0.60 <= action[0] < +0.90:
+            position = next_state[0]
+            if -0.10 <= position < 0.25:
+                reward = 40 - math.pow(action[0], 2) * 0.1
+                # terminacion por salida anticipada
+                print reward
+                self.set_done_reason(2)
+            else:
+                pass
+        else:
+            pass
 
         # TERMINA SI SE QUEDA SIN PRESUPUESTO #################################
         # verifica si se quedo sin prespuesto
--- readme.txt
+++ readme.txt
@@ -1,8 +1,8 @@
     B en repr   | Terminacion por B | Salida Normal | Salida anticipada | Shaping
     Si          | No                | Si            | No                | No
     Si          | No                | Si            | Si                | No
-  X Si          | Si                | Si            | No                | No
-    Si          | Si                | Si            | Si                | No
+    Si          | Si                | Si            | No                | No
+X   Si          | Si                | Si            | Si                | No
     Si          | No                | No            | Si                | No
 
-Exp3. Salida normal y penalizacion por quedarse sin dinero. 
+Exp4. Salida normal y penalizacion por quedarse sin dinero. 
